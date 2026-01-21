160aeec4721673ee617f7d5d90a20751337d788a
NuclearCannon <huangxc_233@qq.com>
2026-01-21 13:24:01 +0800

    重构测试集组织结构以增强可读性

---
+++ tests/basics/main.cpp
@@ -0,0 +1,28 @@
+#include "flints.hpp"
+#include "ntt.hpp"
+#include "twisted_ntter.hpp"
+#include "ziq_array.hpp"
+#include <iostream>
+
+
+int test_ntt_standard();
+int test_ntt_standard_with_ctx();
+int test_ntt_xy();
+int test_ntt_w();
+int test_ziq_ctx();
+int test_circledast();
+
+
+
+int main()
+{
+    int sum = 0;
+    sum += test_ntt_standard();
+    sum += test_ntt_standard_with_ctx();
+    sum += test_ntt_xy();
+    sum += test_ntt_w();
+    sum += test_ziq_ctx();
+    sum += test_circledast();
+
+    std::cout << "Total Pass: " << sum << std::endl; 
+}
