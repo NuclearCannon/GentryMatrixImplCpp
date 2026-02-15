861d0b154c89111413ae0b7c37eaea764f3714eb
NuclearCannon <huangxc_233@qq.com>
2026-02-15 12:53:47 +0800

    将蝴蝶操作独立成单独的函数

--- src/ntt/standard.cpp
+++ src/ntt/standard.cpp
@@ -19,7 +19,70 @@ StandardNTTer::StandardNTTer(size_t n, uint64_t q, uint64_t qroot):
 
 StandardNTTer::~StandardNTTer() = default;
 
+static void _butterfly_inc_mont(
+    uint64_t* a, 
+    const uint64_t* omegas_mont,
+    size_t logn,
+    const MontgomeryMultiplier& mm
+)
+{
+    size_t m = 1;
+    size_t t = logn;
+    const size_t n = 1<<logn;
+    const uint64_t q = mm.getM();
+    while(m<n)
+    {
+        size_t m_half = m;
+        m <<= 1;
+        t--;
+        for(size_t k=0; k<n; k+=m)
+        {
+            for(size_t j=0; j<m_half; j++)
+            {
+                uint64_t w = omegas_mont[(j<<t) & (n-1)];
+                size_t i1 = k+j;
+                size_t i2 = i1 + m_half;
+                uint64_t u = a[i1];
+                uint64_t v = mm.mul(a[i2],w);
+                a[i1] = mod_add(u,v,q);
+                a[i2] = mod_sub(u,v,q);
+            }
+        }
+    }
+}
 
+
+static void _butterfly_dec_mont(
+    uint64_t* a, 
+    const uint64_t* omegas_mont,
+    size_t logn,
+    const MontgomeryMultiplier& mm
+)
+{
+    const size_t n = 1<<logn;
+    size_t m = n;
+    size_t t = 0;
+    const uint64_t q = mm.getM();
+    // 恒有m*t == n/2
+    while (m>1) {
+        size_t m_half = m>>1;
+        for(size_t k=0; k<n; k++)
+        {
+            for(size_t j=k; j<k+m_half; j++)
+            {
+                uint64_t w = omegas_mont[(j<<t) & (n-1)];
+                size_t i1 = j;
+                size_t i2 = j + m_half;
+                uint64_t u = a[i1];
+                uint64_t v = a[i2];
+                a[i1] = mod_add(u,v,q);
+                a[i2] = mm.mul(mod_sub(u,v,q),w);
+            }
+        }
+        m = m_half;
+        t++;
+    }
+}
 void StandardNTTer::_ntt_standard_inner_mont(uint64_t* dst, const uint64_t* src, const uint64_t* roots) const
 {
     assert(src != nullptr && dst != nullptr);
@@ -29,23 +92,7 @@ void StandardNTTer::_ntt_standard_inner_mont(uint64_t* dst, const uint64_t* src,
     for (size_t i = 0; i < n_; ++i) {
         dst[i] = src[rev[i]];
     }
-    size_t m = 1;
-    uint64_t t = n_>>1;
-    // 恒有m*t == n/2
-    while (t) {
-        for (size_t i = 0; i < n_; i += 2 * m) {
-            size_t end = i+m;
-            for (size_t k=0, j=i; j < end; k+=t, ++j) {
-                uint64_t w = roots[k];
-                uint64_t u = dst[j];
-                uint64_t v = mm.mul(dst[j+m], w);
-                dst[j] = mod_add(u, v, q_);
-                dst[j+m] = mod_sub(u, v, q_);         
-            }
-        }
-        m <<= 1;    // m *= 2
-        t >>= 1;
-    }
+    _butterfly_inc_mont(dst, roots, logn_, mm);
 }
 
 void StandardNTTer::ntt_mont(uint64_t* dst, const uint64_t* src) const
