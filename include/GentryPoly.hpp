c2c046da3ffadeabe43aca1a54e3c8ca2b7d082b
NuclearCannon <huangxc_233@qq.com>
2026-02-18 16:36:29 +0800

    另立门户

---
+++ include/GentryPoly.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+// GentryPoly在某个CRT上的分量
+class GPComponent;
+
+// GentryPoly在某个CRT上的分量（CUDA）
+class GPComponentCuda;
+
+// Zq[i,X,Y,W]/<i^2+1, X^n-i, Y^n+i, Phi_p(W)>
+class GentryPoly;
