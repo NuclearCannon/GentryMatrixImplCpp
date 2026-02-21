2d4f184574b45170bf93608dece8063d81a7f21c
NuclearCannon <huangxc_233@qq.com>
2026-02-21 16:33:26 +0800

    优化CPU KS使其具备重构前的性能水平

--- src/FHE/KSGP/key_switch_gp.cpp
+++ src/FHE/KSGP/key_switch_gp.cpp
@@ -21,6 +21,13 @@ KeySwitchKeyGP KeySwitchKeyGP::ksk_gen(const GentryPoly& sk_from, const GentryPo
         cts.push_back(encrypt_gp(sk_from_cp, sk_to, ctx));
         GentryPoly::mul_scalar(sk_from_cp, sk_from_cp, mod);
     }
+    for(auto& pair: cts)
+    {
+        pair.first.ntt(ctx);
+        pair.second.ntt(ctx);
+        GentryPoly::mont_encode(pair.first,  pair.first );
+        GentryPoly::mont_encode(pair.second, pair.second);
+    }
     return KeySwitchKeyGP(std::move(cts), qo);
 }
 std::pair<GentryPoly, GentryPoly> KeySwitchKeyGP::key_switch_big_1(const GentryPoly &a ,const GentryPolyCtx& ctx) const
@@ -28,26 +35,23 @@ std::pair<GentryPoly, GentryPoly> KeySwitchKeyGP::key_switch_big_1(const GentryP
     std::vector<std::vector<uint64_t>> split = GentryPoly(a).split_by_moduli();
 
     GentryPoly ra = GentryPoly::zeros(cts_[0].first.n(), cts_[0].first.p(), cts_[0].first.moduli());
-    GentryPoly rb = ra;
-    GentryPoly rc = ra;
+    GentryPoly rb = GentryPoly::zeros(cts_[0].first.n(), cts_[0].first.p(), cts_[0].first.moduli());
+    GentryPoly piece = GentryPoly::zeros(cts_[0].first.n(), cts_[0].first.p(), cts_[0].first.moduli());
 
     for(int i=0; i<split.size(); i++)
     {
         auto [cta, ctb] = cts_[i];
-        cta.ntt(ctx);
-        ctb.ntt(ctx);
-        GentryPoly piece = GentryPoly::zeros(cta.n(), cta.p(), cta.moduli());
         piece.set_from_vec64(split[i]);
         piece.ntt(ctx);
-        GentryPoly::mul(cta, cta, piece);
-        GentryPoly::mul(ctb, ctb, piece);
+        GentryPoly::mul_mont(cta, cta, piece);
+        GentryPoly::mul_mont(ctb, ctb, piece);
         GentryPoly::add(ra, ra, cta);
         GentryPoly::add(rb, rb, ctb);
     }
     ra.intt(ctx);
     rb.intt(ctx);
-    ra.divmod_by_modulus(rc, qo_);
-    rb.divmod_by_modulus(rc, qo_);
+    ra.divmod_by_modulus(piece, qo_);
+    rb.divmod_by_modulus(piece, qo_);
     ra.drop_modulus(qo_);
     rb.drop_modulus(qo_);
     return std::make_pair(std::move(ra), std::move(rb));
