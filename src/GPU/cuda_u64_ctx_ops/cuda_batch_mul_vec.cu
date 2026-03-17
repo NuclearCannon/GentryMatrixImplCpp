7c60257663fece6bb7aac6bdbbd09b9c9ce51fea
NuclearCannon <huangxc_233@qq.com>
2026-03-17 14:53:57 +0800

    改进cuda_batch_mul_vec效率

--- src/GPU/cuda_u64_ctx_ops/cuda_batch_mul_vec.cu
+++ src/GPU/cuda_u64_ctx_ops/cuda_batch_mul_vec.cu
@@ -3,38 +3,24 @@
 #include "GPU/cuda_check.hpp"
 #include "montgomery.hpp"
 
-// 用这组参数就没问题
-constexpr int THREAD_PER_GROUP = 32;
-constexpr int ROWS_PER_THREAD = 32;
-
-
-// 暂行方案：
-// 每个线程负责一行
-// 共开batch_size个线程
-__global__ void _cuda_batch_mul_vec_kernel(
-    uint64_t* dst,
-    const uint64_t* src,
-    const uint64_t* vec,
-    size_t batch_size,
-    size_t vec_len,
+// 核函数：执行 dst[i] = src[i] * vec[i % vec_len]
+__global__ void _cuda_batch_mul_vec_kernel_ver2(
+    uint64_t* __restrict__ dst,
+    const uint64_t* __restrict__ src,
+    const uint64_t* __restrict__ vec,
+    int vec_len,
+    int total_size,
     uint64_t M,
     uint64_t N1
 )
 {
-    unsigned gid = blockIdx.x;
-    unsigned tid = threadIdx.x;
-    unsigned id = gid * blockDim.x + tid;
-    if(id < batch_size)
-    {
-        dst += id * vec_len;
-        src += id * vec_len;
-        for(size_t i=0; i<vec_len; i++)
-        {
-            dst[i] = _mul_cuda(src[i], vec[i], M, N1);
-        }
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < total_size) {
+        int vec_idx = idx % vec_len;
+        dst[idx] = _mul_cuda(src[idx], vec[vec_idx], M, N1);
     }
-    
 }
+
 
 
 float cuda_batch_mul_vec(
@@ -50,19 +36,21 @@ float cuda_batch_mul_vec(
     assert(src.size() == batch_size*vec_len*sizeof(uint64_t));
     assert(vec.size() == vec_len*sizeof(uint64_t));
     
-    dim3 blockSize(THREAD_PER_GROUP);
-    dim3 gridSize((batch_size + ROWS_PER_THREAD - 1) / ROWS_PER_THREAD);
 
     // cudaEvent_t start, stop;
     // cudaEventCreate(&start);
     // cudaEventCreate(&stop);
     // cudaEventRecord(start);
+    size_t total_size = batch_size * vec_len;
+    int blockSize = 256;
+    int gridSize = (total_size + blockSize - 1) / blockSize;
 
-    _cuda_batch_mul_vec_kernel<<<gridSize, blockSize>>>(
+    _cuda_batch_mul_vec_kernel_ver2<<<gridSize, blockSize>>>(
         (uint64_t*)dst.get_ptr(),
         (const uint64_t*)src.get_ptr(),
         (const uint64_t*)vec.get_ptr(),
-        batch_size, vec_len,
+        vec_len,
+        total_size, 
         mm.M, mm.N1
     );
 
